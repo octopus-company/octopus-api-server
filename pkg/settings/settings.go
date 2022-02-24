eac96593a34508dbfae522725fd28a119552a102
lisy <lisywork@qq.com>
2022-02-24 09:13:41 +0800

    如果ui-index是local,就用ui-path定义的路径,所以容易打包的时候要注意

--- pkg/settings/settings.go
+++ pkg/settings/settings.go
@@ -16,8 +16,8 @@ var (
 	ServerURL        = NewSetting("server-url", "")
 	Location         = NewSetting("location", "")
 	SystemNamespaces = NewSetting("system-namespaces", "kube-system,kube-public,octopus-system")
-	UIIndex          = NewSetting("ui-index", "https://rancher-octopus.s3-accelerate.amazonaws.com/ui/latest/index.html")
-	UIPath           = NewSetting("ui-path", "")
+	UIIndex          = NewSetting("ui-index", "local")
+	UIPath           = NewSetting("ui-path", "/usr/share/rancher/octopus-ui")
 	APIUIVersion     = NewSetting("api-ui-version", "1.1.9") // Please update the OCTOPUS_API_UI_VERSION in package/Dockerfile when updating the version here.
 )
 
